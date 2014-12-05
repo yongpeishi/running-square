a7ebd535bf22c444cec18257a1da069b56b7b345
yongpeishi <yongpeishi@gmail.com>
2014-12-05 16:32:10 +1100

    change moving dir when hit spacebar

--- src/hello_quil/core.clj
+++ src/hello_quil/core.clj
@@ -9,39 +9,50 @@
   {:x 10
    :y 10
    :x-dir 1
-   :y-dir 0})
+   :y-dir 0
+   :dir 1})
 
 (defn draw [state]
   (let [x (:x state)
         y (:y state)]
         (q/rect x y 150 100)))
 
-(defn change-dir [coor]
-  (case coor
-    [10 10] {:new-x-dir 1 :new-y-dir 0}
-    [340 10] {:new-x-dir 0 :new-y-dir 1}
-    [340 390] {:new-x-dir -1 :new-y-dir 0}
-    [10 390] {:new-x-dir 0 :new-y-dir -1}
-    nil))
+(defn change-xy [dir coor]
+  (if (= dir 1)
+    (case coor
+      [10 10] {:new-x-dir 1 :new-y-dir 0}
+      [340 10] {:new-x-dir 0 :new-y-dir 1}
+      [340 390] {:new-x-dir -1 :new-y-dir 0}
+      [10 390] {:new-x-dir 0 :new-y-dir -1}
+      nil)
+    (case coor
+      [10 10] {:new-x-dir 0 :new-y-dir 1}
+      [340 10] {:new-x-dir -1 :new-y-dir 0}
+      [340 390] {:new-x-dir 0 :new-y-dir -1}
+      [10 390] {:new-x-dir 1 :new-y-dir 0}
+      nil)))
+
 
 (defn update [state]
-  (let [{:keys [x y x-dir y-dir]} state
+  (let [{:keys [x y x-dir y-dir dir]} state
 
-        {:keys [new-x-dir new-y-dir]} (or (change-dir [x y])
+        {:keys [new-x-dir new-y-dir]} (or (change-xy dir [x y])
                                         {:new-x-dir x-dir :new-y-dir y-dir})]
     {:x (+ x new-x-dir)
      :y (+ y new-y-dir)
      :x-dir new-x-dir
-     :y-dir new-y-dir}))
+     :y-dir new-y-dir
+     :dir dir}))
 
 (defn key-pressed [state event]
-  (let [{:keys [x y x-dir y-dir]} state]
+  (let [{:keys [x y x-dir y-dir dir]} state]
     (println (q/key-code))
     (if (= 32 (q/key-code))
       {:x x
        :y y
        :x-dir (* x-dir -1)
-       :y-dir (* y-dir -1)})))
+       :y-dir (* y-dir -1)
+       :dir (* dir -1)})))
 
 (q/defsketch my-square
   :title "my sqr"
